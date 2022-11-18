cf4cdc12f44a3214420b7f06ad01e888e8cc9c02
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-18 15:22:30 +0100

    exercise request finalised

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ let nome = prompt ('che me dici er nome tuo?');
 let cognome = prompt ('er cognome invece?');
 let colorePreferito = prompt ('un colore che te piace tanto?');
 
+document.getElementById('psw').innerHTML = "tiè, beccate sta password da paura!: " + nome + cognome + colorePreferito + "22"
